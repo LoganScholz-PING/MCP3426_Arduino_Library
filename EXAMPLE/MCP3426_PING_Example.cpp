09f01b5de0b4e85ec1fad52b5ac8ab6de7c3d4f4
Logan Scholz <80925426+LoganScholz-PING@users.noreply.github.com>
2022-05-20 15:07:27 -0700

    Update MCP3426_PING_Example.cpp
    added channel 2 and left notes about how you don't need to initialize the chip anymore (calling a read function will do it for you now, because it needs to write the current channel to the config register anyway)

--- EXAMPLE/MCP3426_PING_Example.cpp
+++ EXAMPLE/MCP3426_PING_Example.cpp
@@ -15,22 +15,49 @@ void setup() {
   
   Serial.println(F("[INFO] Welcome MCP3426 Communication Utility. Sending Config Packet..."));
   if(!initializeMCP3426()) for(;;) // couldn't init MCP3426 so we're done here
+	                           // NOTE that calling initializeMCP3426() with no parameters 
+	                           // sets channel 1 as default read channel
+	                           // call initalizeMCP3426(1); to set config to auto read channel 1
+	                           // call initalizeMCP3426(2); to set config to auto read channel 2
+	                           // - you don't NEED to initialize though, readMCP3426([channel here]) writes config packet
   
   delaySafeMillis(100); // give MCP3426 some time to settle
 }
 
 void loop() {
-  uint8_t config       = readMCP3426Config();
-  int16_t full_reading = readMCP3426CurrentBits();
-  double  voltage      = readMCP3426CurrentVoltage();
-  
-  Serial.print(F("[INFO] Config = 0x"));
-  Serial.print(config, HEX);
-  Serial.print(F(" // Full Reading = 0x"));
-  Serial.print(full_reading, HEX);
-  Serial.print(F(" // Voltage = "));
-  Serial.print(voltage);
+  // READ CHANNEL 1 AND PRINT
+  Serial.println("*********************************");
+  Serial.println("*********************************");
+
+  int16_t full_reading_ch1 = readMCP3426CurrentBits(1);
+  double voltage_ch1 = readMCP3426CurrentVoltage(1);
+  uint8_t config_ch1 = readMCP3426Config();
+  Serial.print(F("[INFO] CH1 Config = 0x"));
+  Serial.print(config_ch1, HEX);
+  Serial.print(F(" (0B"));
+  Serial.print(config_ch1, BIN);
+  Serial.print(F(") // CH1 Full Reading = 0x"));
+  Serial.print(full_reading_ch1, HEX);
+  Serial.print(F(" // CH1 Voltage = "));
+  Serial.print(voltage_ch1);
   Serial.println(F(" Volts"));
-  
+
+  // READ CHANNEL 2 AND PRINT
+  delaySafeMillis(50); // don't need this
+
+  int16_t full_reading_ch2 = readMCP3426CurrentBits(2);
+  double voltage_ch2 = readMCP3426CurrentVoltage(2);
+  uint8_t config_ch2 = readMCP3426Config();
+
+  Serial.print(F("[INFO] CH2 Config = 0x"));
+  Serial.print(config_ch2, HEX);
+  Serial.print(F(" (0B"));
+  Serial.print(config_ch2, BIN);
+  Serial.print(F(") // CH2 Full Reading = 0x"));
+  Serial.print(full_reading_ch2, HEX);
+  Serial.print(F(" // CH2 Voltage = "));
+  Serial.print(voltage_ch2);
+  Serial.println(F(" Volts"));
+
   delaySafeMillis(1000); // don't blow up our serial monitor
 }
